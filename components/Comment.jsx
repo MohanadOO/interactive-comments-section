29a8bbb9e0c51bbc6fdbf0f55413608e2cdb65d8
MohanadOO <moohand220@gmail.com>
2022-03-25 21:16:35 +0300

    responsive layout adjustment

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -18,39 +18,65 @@ export default function Comment(props) {
   }
 
   return (
-    <div className='comment'>
-      <div className='user-info'>
-        <img className='user-logo' src={props.image} />
-        <p className='user-name'>{props.username}</p>
-        {props.currentUser && <p className='you-card'>you</p>}
-        <p className='comment-date'>{props.createdAt}</p>
+    <div className='comment flex-desktop'>
+      <div className='comment-rate comment-rate-flex-column'>
+        <img onClick={increaseScore} src='../images/icon-plus.svg' />
+        <p>{score}</p>
+        <img onClick={decreaseScore} src='../images/icon-minus.svg' />
       </div>
-      <p className='comment-message'>{props.content}</p>
-      <div className='comment-info'>
-        <div className='comment-rate'>
-          <img onClick={increaseScore} src='../images/icon-plus.svg' />
-          <p>{score}</p>
-          <img onClick={decreaseScore} src='../images/icon-minus.svg' />
-        </div>
-        {props.currentUser ? (
-          <div className='comment-edit'>
-            <span onClick={props.handleDelete}>
-              <img src='../images/icon-delete.svg' />
-              <span className='comment-edit-delete'>Delete</span>
-            </span>
-            <span onClick={props.handleEdit}>
-              <img src='../images/icon-edit.svg' />
-              <span className='comment-edit-edit'>Edit</span>
-            </span>
-          </div>
-        ) : (
-          <div className='comment-edit'>
-            <div onClick={props.handleReply}>
-              <img src='../images/icon-reply.svg' />
-              <span className='comment-edit-reply'>Reply</span>
+      <div>
+        <div className='user-info'>
+          <img className='user-logo' src={props.image} />
+          <p className='user-name'>{props.username}</p>
+          {props.currentUser && <p className='you-card'>you</p>}
+          <p className='comment-date'>{props.createdAt}</p>
+          {props.currentUser ? (
+            <div className='comment-edit hide'>
+              <span onClick={props.handleDelete}>
+                <img src='../images/icon-delete.svg' />
+                <span className='comment-edit-delete'>Delete</span>
+              </span>
+              <span onClick={props.handleEdit}>
+                <img src='../images/icon-edit.svg' />
+                <span className='comment-edit-edit'>Edit</span>
+              </span>
             </div>
+          ) : (
+            <div className='comment-edit hide'>
+              <div onClick={props.handleReply}>
+                <img src='../images/icon-reply.svg' />
+                <span className='comment-edit-reply'>Reply</span>
+              </div>
+            </div>
+          )}
+        </div>
+        <p className='comment-message'>{props.content}</p>
+        <div className='comment-info'>
+          <div className='comment-rate show-flex'>
+            <img onClick={increaseScore} src='../images/icon-plus.svg' />
+            <p>{score}</p>
+            <img onClick={decreaseScore} src='../images/icon-minus.svg' />
           </div>
-        )}
+          {props.currentUser ? (
+            <div className='comment-edit show'>
+              <span onClick={props.handleDelete}>
+                <img src='../images/icon-delete.svg' />
+                <span className='comment-edit-delete'>Delete</span>
+              </span>
+              <span onClick={props.handleEdit}>
+                <img src='../images/icon-edit.svg' />
+                <span className='comment-edit-edit'>Edit</span>
+              </span>
+            </div>
+          ) : (
+            <div className='comment-edit show'>
+              <div onClick={props.handleReply}>
+                <img src='../images/icon-reply.svg' />
+                <span className='comment-edit-reply'>Reply</span>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
--- components/Reply.jsx
+++ components/Reply.jsx
@@ -20,40 +20,69 @@ export default function Reply(props) {
     <div className='reply'>
       <hr className='reply-divider' />
       <div className='reply-section'>
-        <div className='user-info'>
-          <img className='user-logo' src={props.image} />
-          <p className='user-name'>{props.username}</p>
-          {props.currentUser && <p className='you-card'>you</p>}
-          <p className='comment-date'>{props.createdAt}</p>
-        </div>
-        <p className='comment-message'>
-          <span className='mention'>@{props.replyingTo}</span> {props.content}
-        </p>
-        <div className='comment-info'>
-          <div className='comment-rate'>
+        <div className='comment flex-desktop'>
+          <div className='comment-rate comment-rate-flex-column'>
             <img onClick={increaseScore} src='../images/icon-plus.svg' />
             <p>{score}</p>
             <img onClick={decreaseScore} src='../images/icon-minus.svg' />
           </div>
-          {props.currentUser ? (
-            <div className='comment-edit'>
-              <span onClick={props.handleDelete}>
-                <img src='../images/icon-delete.svg' />
-                <span className='comment-edit-delete'>Delete</span>
-              </span>
-              <span onClick={props.handleEdit}>
-                <img src='../images/icon-edit.svg' />
-                <span className='comment-edit-edit'>Edit</span>
-              </span>
+          <div>
+            <div className='user-info'>
+              <img className='user-logo' src={props.image} />
+              <p className='user-name'>{props.username}</p>
+              {props.currentUser && <p className='you-card'>you</p>}
+              <p className='comment-date'>{props.createdAt}</p>
+              {props.currentUser ? (
+                <div className='comment-edit hide'>
+                  <span onClick={props.handleDelete}>
+                    <img src='../images/icon-delete.svg' />
+                    <span className='comment-edit-delete'>Delete</span>
+                  </span>
+                  <span onClick={props.handleEdit}>
+                    <img src='../images/icon-edit.svg' />
+                    <span className='comment-edit-edit'>Edit</span>
+                  </span>
+                </div>
+              ) : (
+                <div className='comment-edit hide'>
+                  <div onClick={props.handleReply}>
+                    <img src='../images/icon-reply.svg' />
+                    <span className='comment-edit-reply'>Reply</span>
+                  </div>
+                </div>
+              )}
             </div>
-          ) : (
-            <div className='comment-edit'>
-              <div onClick={props.handleReply}>
-                <img src='../images/icon-reply.svg' />
-                <span className='comment-edit-reply'>Reply</span>
+            <p className='comment-message'>
+              <span className='mention'>@{props.replyingTo}</span>{' '}
+              {props.content}
+            </p>
+            <div className='comment-info'>
+              <div className='comment-rate show-flex'>
+                <img onClick={increaseScore} src='../images/icon-plus.svg' />
+                <p>{score}</p>
+                <img onClick={decreaseScore} src='../images/icon-minus.svg' />
               </div>
+              {props.currentUser ? (
+                <div className='comment-edit show'>
+                  <span onClick={props.handleDelete}>
+                    <img src='../images/icon-delete.svg' />
+                    <span className='comment-edit-delete'>Delete</span>
+                  </span>
+                  <span onClick={props.handleEdit}>
+                    <img src='../images/icon-edit.svg' />
+                    <span className='comment-edit-edit'>Edit</span>
+                  </span>
+                </div>
+              ) : (
+                <div className='comment-edit show'>
+                  <div onClick={props.handleReply}>
+                    <img src='../images/icon-reply.svg' />
+                    <span className='comment-edit-reply'>Reply</span>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
       </div>
     </div>
