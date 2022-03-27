7383ecc34044a2f96e05bd8e73d9fde49d33cd15
MohanadOO <moohand220@gmail.com>
2022-03-27 17:36:23 +0300

    Responsiveness adjustment

--- components/AddComment.jsx
+++ components/AddComment.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function AddComment(props) {
   return (
     <div className='add-comment'>
+      <img className='author-logo hide' src={props.currentUser} />
       <textarea
         placeholder={props.textarea_placeholder}
         value={props.textarea_value}
@@ -11,9 +12,9 @@ function AddComment(props) {
         {props.comment}
       </textarea>
       <div className='send-info'>
-        <img className='author-logo' src={props.currentUser} />
+        <img className='author-logo show' src={props.currentUser} />
         <button onClick={props.handleComment} className='send-btn'>
-          Send
+          {props.sendButton}
         </button>
       </div>
     </div>
